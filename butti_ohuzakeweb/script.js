e97bcc4ad591d5455c3c834f14179110f3074710
yukkuriremiria <98326743+yukkuriremiria@users.noreply.github.com>
2022-12-14 23:42:27 +0900

    Add files via upload

---
+++ butti_ohuzakeweb/script.js
@@ -0,0 +1,17 @@
+(function (window, $) {
+  'use strict';
+
+  $.fn.useSound = function (_event, _id) {
+    var se = $(_id);
+    this.on(_event, function(){
+      se[0].currentTime = 0;
+      se[0].play();
+      return false;
+    });
+  };
+  
+})(this, this.jQuery);
+
+
+//Execute
+$('#btn').useSound('click', '#btnsound');
