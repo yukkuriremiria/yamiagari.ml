b6c9664700f7fc707433db49a241e6da6952a0dd
yukkuriremiria <98326743+yukkuriremiria@users.noreply.github.com>
2023-01-15 00:26:02 +0900

    Add files via upload

---
+++ kazu-server/js.js
@@ -0,0 +1,12 @@
+// クラス変更
+window.onload = function () {
+    const spinner = document.getElementById('loading');
+    spinner.classList.add('loaded');
+}
+
+//safari対策
+window.onpageshow = function (event) {
+    if (event.persisted) {
+        window.location.reload();
+    }
+};
