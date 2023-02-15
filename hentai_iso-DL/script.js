15317321a8d31cfb8b52b5b6eaef30f06ed27915
yukkuriremiria <98326743+yukkuriremiria@users.noreply.github.com>
2023-02-15 22:27:28 +0900

    Add files via upload

---
+++ hentai_iso-DL/script.js
@@ -0,0 +1,13 @@
+document.querySelector("img.x.youtubelogo")
+.addEventListener('click',() =>{
+  <!--
+window.open("https://x.gd/nk0U0");
+//-->
+})
+
+document.querySelector("img.x.twitterlogo")
+.addEventListener('click',() =>{
+    <!--
+window.open("https://x.gd/abVEq");
+//-->
+})
