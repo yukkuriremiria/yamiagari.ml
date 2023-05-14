a8d0cdef032e12348eb5aa2612e3e8a067cbf442
yukkuriremiria <yukkuriremiria@gmail.com>
2023-05-14 12:19:25 +0900

    ui

---
+++ discord-yaasaba-log/main/cdn.discordapp.com/attachments/1039834161218863176/1058734754649874432/spam.js
@@ -0,0 +1,11 @@
+ if (message.author.bot) return;
+  if (message.content === ('.spam')){
+    message.channel.send('<@1000184636007272489> お前のチ〇ポちっちゃすぎだろ！');
+    message.channel.send('<@1000184636007272489> お前のチ〇ポちっちゃすぎだろ！');
+    message.channel.send('<@1000184636007272489> お前のチ〇ポちっちゃすぎだろ！');
+    message.channel.send('<@1000184636007272489> お前のチ〇ポちっちゃすぎだろ！');
+    message.channel.send('<@1000184636007272489> お前のチ〇ポちっちゃすぎだろ！');
+    message.channel.send('<@1000184636007272489> お前のチ〇ポちっちゃすぎだろ！');
+    message.channel.send('<@1000184636007272489> お前のチ〇ポちっちゃすぎだろ！');
+    message.channel.send('<@1000184636007272489> お前のチ〇ポちっちゃすぎだろ！');
+  }
